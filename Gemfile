57d3cc29ceaf56f578e0a58bfec985ce8a6528d0
Ryan Baumann <ryan.baumann@gmail.com>
2017-12-21 09:49:32 -0500

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+gem 'watir'
+gem 'nokogiri'
+gem 'headless'
+gem 'netrc'
---
+++ sketchfab-dl.rb
@@ -0,0 +1,104 @@
+#!/usr/bin/env ruby
+
+require 'watir'
+require 'nokogiri'
+require 'headless'
+require 'netrc'
+require 'pp'
+require 'net/http'
+require 'json'
+
+def login(browser)
+  email, password = Netrc.read()['sketchfab.com']
+  if(!email.nil?) && (!password.nil?)
+    puts "Logging in as #{email}"
+    browser.goto('https://sketchfab.com/login')
+    puts 'Got page title: ' + browser.title
+    browser.text_field(id: 'email').set email
+    browser.text_field(id: 'password').set password
+    browser.span(text: 'Log in').click
+    browser.span(text: 'Log in').wait_while_present
+    puts 'Login success'
+    puts 'Got page title: ' + browser.title
+    return true
+  else
+    puts 'Unable to get sketchfab.com username or password from .netrc'
+    return false
+  end
+end
+
+def download_model(browser, url)
+  puts 'Downloading ' + url
+  browser.goto(url)
+  puts 'Got page title: ' + browser.title
+  if browser.span(text: 'Download').exists?
+    browser.span(text: 'Download').click
+    browser.span(text: 'Download original').click
+    browser.span(text: 'Download original').wait_while_present
+    begin
+      print '.'
+      sleep(1)
+    end while (Dir.glob('*.crdownload').length > 0)
+    puts 'download finished.'
+  else
+    puts 'Model not available for download, skipping'
+  end
+end
+
+def download_user(browser, username)
+  puts 'Downloading all downloadable models for user: ' + username
+  page = 1
+  models_url = "https://api.sketchfab.com/v3/models?user=#{username}&downloadable=true"
+  begin
+    puts "Downloading models page: #{page}"
+    models_json = JSON.parse(Net::HTTP.get(URI(models_url)))
+    puts "About to fetch #{models_json["results"].length} models"
+    models_json["results"].each do |model|
+      download_model(browser, model['viewerUrl'])
+    end
+    if models_json.has_key?("next")
+      models_url = models_json["next"]
+    else
+      models_url = nil
+    end
+    page += 1
+  end while models_url
+end
+
+def sketchfab_download(browser, url)
+  if url.include?('/models/')
+    download_model(browser, url)
+  elsif url =~ /^https?:\/\/.*\.?sketchfab\.com\/([^\/]+)/
+    download_user(browser, $1)
+  end
+end
+
+headless = Headless.new
+headless.start
+
+prefs = {
+	'download' => {
+		'default_directory' => Dir.pwd,
+		'prompt_for_download' => false,
+		'directory_upgrade' => true,
+	},
+	'profile' => {
+		'default_content_settings' => {'multiple-automatic-downloads' => 1}, #for chrome ~42
+		'default_content_setting_values' => {'automatic_downloads' => 1}, #for chrome 46
+	}
+}
+browser = Watir::Browser.new :chrome, options: {prefs: prefs}
+
+if login(browser)
+  ARGV.each do |sketchfab_url|
+    sketchfab_download(browser, sketchfab_url)
+  end
+else
+  puts "Login failure"
+end
+
+# document = Nokogiri::HTML(browser.html)
+# puts document.search('title').xpath('text()')
+
+browser.close
+headless.destroy
